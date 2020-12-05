2e2ca6a8f04f83d7f7b0c9bf1452f1b5a0fc0f93
Yunus0or1 <ahmedyunuspilot@gmail.com>
2020-12-05 11:18:52 +0600

    Project pharmacy initiated with config

---
+++ pharmacy_app/android/app/src/main/java/com/arbree/pharmacy_app/Application.java
@@ -0,0 +1,25 @@
+package com.arbree.pharmacy_app;
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    }
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    }
+}
