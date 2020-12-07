e55304fd847fbb79e252e0b89c9da94f16fe3d1c
Yunus0or1 <ahmedyunuspilot@gmail.com>
2020-12-07 10:01:55 +0600

    . is added in home slider

--- pharmacy_app/lib/src/component/cards/carousel_slider_card.dart
+++ pharmacy_app/lib/src/component/cards/carousel_slider_card.dart
@@ -3,7 +3,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pharmacy_app/src/component/cards/homepage_slider_single_card.dart';
 
-class CarouselSliderCard extends StatelessWidget {
+class CarouselSliderCard extends StatefulWidget {
+  @override
+  _CarouselSliderCardState createState() => _CarouselSliderCardState();
+}
+
+class _CarouselSliderCardState extends State<CarouselSliderCard> {
+  final listItems = [1, 2, 3];
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,23 +29,50 @@ class CarouselSliderCard extends StatelessWidget {
   }
 
   Widget buildBody() {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: 150,
-        aspectRatio: 3,
-        viewportFraction: 1,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        reverse: false,
-        autoPlay: true,
-      ),
-      items: [1, 2, 3].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return HomePageSliderSingleCard();
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 150,
+            aspectRatio: 3,
+            viewportFraction: 1,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            onPageChanged: (index,reason){
+              setState(() {
+                _currentIndex = index;
+              });
+            }
+
+          ),
+          items: listItems.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return HomePageSliderSingleCard();
+              },
+            );
+          }).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: listItems.map((image) {       //these two lines
+            int index=listItems.indexOf(image); //are changed
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == index
+                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                      : Color.fromRGBO(0, 0, 0, 0.4)),
+            );
           },
-        );
-      }).toList(),
+          ).toList(),
+        ),
+      ],
     );
   }
 }
