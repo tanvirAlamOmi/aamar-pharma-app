7a1962669fca264fed2504ee1668d428e9000861
Yunus <ahmedyunuspilot@gmail.com>
2021-03-18 17:32:33 +0600

    updated

---
+++ pharmacy_app/lib/src/pages/confirm_order_success_with_repeat_order.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pharmacy_app/src/component/general/app_bar_back_button.dart';
+import 'package:pharmacy_app/src/component/general/common_ui.dart';
+import 'package:pharmacy_app/src/util/util.dart';
+
+class ConfirmOrderSuccessWithRepeatOrder extends StatelessWidget {
+  final IconData icon;
+  final String pageTitle;
+  final String title;
+  final String message;
+
+  const ConfirmOrderSuccessWithRepeatOrder(
+      {Key key, this.icon, this.title, this.message, this.pageTitle})
+      : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: AppBarBackButtonCross(),
+          elevation: 1,
+          centerTitle: true,
+          title: CustomText(pageTitle,
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        body: buildBody());
+  }
+
+  Widget buildBody() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          child: Icon(
+            icon,
+            size: 80,
+            color: Util.greenishColor(),
+          ),
+        ),
+        SizedBox(height: 30),
+        Container(
+          child: CustomText(title,
+              color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 17),
+        ),
+        Container(
+          width: 200,
+          alignment: Alignment.center,
+          child: CustomText(message,
+              textAlign: TextAlign.center, color: Colors.grey),
+        )
+      ],
+    );
+  }
+}
