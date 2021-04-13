5683e44f15c6393fe18b82893c11c4807317f99f
Yunus0or1 <ahmedyunuspilot@gmail.com>
2021-04-13 15:37:21 +0600

    updated

--- pharmacy_app/ios/Runner/AppDelegate.m
+++ pharmacy_app/ios/Runner/AppDelegate.m
@@ -6,6 +6,9 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    if (@available(iOS 10.0, *)) {
+      [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
+    }
   [FIRApp configure];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
