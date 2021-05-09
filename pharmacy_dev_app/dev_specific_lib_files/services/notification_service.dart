0d08dea1b5285c0fef4b7022fd7b088b5dd6af72
Yunus <ahmedyunuspilot@gmail.com>
2021-05-10 01:18:00 +0600

    Updated Default FIle in Dev Apk

--- pharmacy_dev_app/dev_specific_lib_files/services/notification_service.dart
+++ pharmacy_dev_app/dev_specific_lib_files/services/notification_service.dart
@@ -1,22 +1,19 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/services.dart';
 import 'package:pharmacy_app/src/bloc/stream.dart';
 import 'package:pharmacy_app/src/configs/server_config.dart';
+import 'package:pharmacy_app/src/models/general/App_Enum.dart';
 import 'package:pharmacy_app/src/models/states/app_vary_states.dart';
 import 'package:pharmacy_app/src/models/states/event.dart';
 import 'package:pharmacy_app/src/store/store.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:pharmacy_app/src/util/util.dart';
+import 'package:launch_review/launch_review.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print('on onBackgroundMessage ${message.data}');
+  print('onBackgroundMessage Execution');
   // Do not navigate route from here.
 }
 
@@ -37,7 +34,6 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 Future<void> firebaseCloudMessagingListeners() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  print("FIREBASE  initialized");
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   await flutterLocalNotificationsPlugin
@@ -66,13 +62,17 @@ Future<void> firebaseCloudMessagingListeners() async {
       FirebaseMessaging.instance
           .subscribeToTopic("pharma-push-dev-service-ios");
 
+    if (ServerConfig.environmentMode == "dev")
+      FirebaseMessaging.instance
+          .subscribeToTopic("pharma-push-dev-service-dev");
+
     FirebaseMessaging.instance
         .getInitialMessage()
         .then((RemoteMessage message) {
       // On click to start the app from beginning, this is executed.
       if (message != null) {
-        print('on getInitialMessage $message.data');
-        navigateToSpecificScreen();
+        print('getInitialMessage Execution');
+        navigateToSpecificScreen(data: message.data, pushMessageTapped: true);
       }
     });
 
@@ -97,14 +97,14 @@ Future<void> firebaseCloudMessagingListeners() async {
                   presentSound: true,
                 )));
       }
-      print('on onMessage ${message.data['code']}');
-      navigateToSpecificScreen();
+      print('onMessage Execution');
+      navigateToSpecificScreen(data: message.data, pushMessageTapped: false);
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((message) async {
       // On click the push message this is executed.
-      print('on onMessageOpenedApp ${message.data}');
-      navigateToSpecificScreen();
+      print('onMessageOpenedApp Execution');
+      navigateToSpecificScreen(data: message.data, pushMessageTapped: true);
     });
   } catch (error) {
     print("ERROR in FCM Service");
@@ -112,17 +112,74 @@ Future<void> firebaseCloudMessagingListeners() async {
   }
 }
 
-void navigateToSpecificScreen() async {
-  AppVariableStates.instance.navigatorKey.currentState
-      .pushNamedAndRemoveUntil('/main', (Route<dynamic> route) => false);
-  AppVariableStates.instance.navigatorKey.currentState
-      .pushNamed('/notification');
-  await Future.delayed(Duration(seconds: 2));
-  Streamer.putEventStream(Event(EventType.REFRESH_ALL_PAGES));
+void navigateToSpecificScreen({dynamic data, bool pushMessageTapped}) async {
+  print('PUSH DATA: ' + data['SERVER_DATA']);
+  await Future.delayed(Duration(milliseconds: 2000));
+  Streamer.putEventStream(Event(EventType.REFRESH_ORDER_PAGE));
+
+  print('PAGE NAME: ' + AppVariableStates.instance.pageName);
+
+  if (pushMessageTapped) {
+    print('PUSH MESSAGE TAPPED');
+    AppVariableStates.instance.navigatorKey.currentState
+        .pushNamedAndRemoveUntil('/main', (Route<dynamic> route) => false);
+    AppVariableStates.instance.navigatorKey.currentState
+        .pushNamed('/notification');
+    return;
+  }
+
+  switch (AppVariableStates.instance.pageName) {
+    case AppEnum.PAGE_ADD_ITEMS:
+      break;
+
+    case AppEnum.PAGE_ADD_NEW_ADDRESS:
+      break;
+
+    case AppEnum.PAGE_CONFIRM_INVOICE:
+      break;
+
+    case AppEnum.PAGE_CONFIRM_ORDER:
+      break;
+
+    case AppEnum.PAGE_INITIAL_TUTORIAL_SCROLLING:
+      break;
+
+    case AppEnum.PAGE_ORDER_FINAL_INVOICE:
+      break;
+
+    case AppEnum.PAGE_REPEAT_ORDER_CHOICE:
+      break;
+
+    case AppEnum.PAGE_SPECIAL_REQUEST_PRODUCT:
+      break;
+
+    case AppEnum.PAGE_UPLOAD_PRESCRIPTION_VERIFY:
+      break;
+
+    case AppEnum.PAGE_VERIFICATION:
+      break;
+
+    default:
+      switch (data['SERVER_DATA']) {
+        case 'ANYTHING':
+          break;
+
+        case 'UPDATE_APP':
+          LaunchReview.launch(
+              androidAppId: 'com.arbree.aamarpharma', iOSAppId: '123');
+          break;
+
+        default:
+          AppVariableStates.instance.navigatorKey.currentState
+              .pushNamedAndRemoveUntil(
+                  '/main', (Route<dynamic> route) => false);
+          AppVariableStates.instance.navigatorKey.currentState
+              .pushNamed('/notification');
+          break;
+      }
+  }
 }
 
 void iOSPermission() {
   FirebaseMessaging.instance.requestPermission();
 }
-
-void processNotificationMessage(Map<String, dynamic> message) async {}
